d582900447cbb988f3c6392e7bcd2fc5fae921d8
Carlos Roig <carroisal3@gmail.com>
2015-04-05 23:17:42 +0200

    Initial Commit

---
+++ CRSMediaImporterExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CRSMediaImporterExample
+//
+//  Created by Carlos Roig on 05/04/15.
+//  Copyright (c) 2015 CRoigSalvador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
